4a2b9aaf1ff6c005bd6eb4e70c949a34a9f2d079
Ruta-B <baumaneruta38@gmail.com>
2024-08-30 20:20:23 +0200

    inject moment.js, update current time in js

--- java/java.js
+++ java/java.js
@@ -1,1 +1,18 @@
+function getTime() {
+  let currentTimeZone = moment().tz("Europe/Paris");
+  let date = currentTimeZone.format("dddd, MMMM Do, yyyy");
+  let time = currentTimeZone.format(" H:mm:ss [<small>]A[</small]");
+  let dateParis = document.querySelector("#date-paris");
+  let dateRiga = document.querySelector("#date-riga");
+  let timeParis = document.querySelector("#time-paris");
+  let timeRiga = document.querySelector("#time-riga");
+  timeParis.innerHTML = time;
+  timeRiga.innerHTML = time;
+  dateParis.innerHTML = date;
+  dateRiga.innerHTML = date;
+}
 
+getTime();
+setInterval(getTime, 1);
+/*let selectElement = document.querySelector("#cities");
+selectElement.addEventListener("change", getTime);*/
