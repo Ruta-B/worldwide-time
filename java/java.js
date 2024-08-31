54e8a82b3db1dcae6d57f2d804769fed13f9b3d8
Ruta-B <baumaneruta38@gmail.com>
2024-08-31 19:53:31 +0200

    create select option function in js

--- java/java.js
+++ java/java.js
@@ -12,7 +12,27 @@ function getTime() {
   dateRiga.innerHTML = date;
 }
 
+function updateTime(event) {
+  let chosenCity = event.target.value;
+  let getCity = chosenCity.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(chosenCity);
+  let currentCity = document.querySelector("#chosen-city");
+  currentCity.innerHTML = `
+  <div class="city">
+        <div>
+          <h2>${getCity}</h2>
+          <div class="date" id="date-paris">${cityTime.format(
+            "dddd, MMMM Do, yyyy"
+          )}</div>
+        </div>
+        <div class="time" id="time-paris">${cityTime.format(
+          " H:mm:ss [<small>]A[</small]"
+        )}</div>
+      </div>
+`;
+}
+
 getTime();
 setInterval(getTime, 1);
-/*let selectElement = document.querySelector("#cities");
-selectElement.addEventListener("change", getTime);*/
+let selectElement = document.querySelector("#cities");
+selectElement.addEventListener("change", updateTime);
